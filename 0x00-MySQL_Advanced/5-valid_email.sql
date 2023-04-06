995819a288de2a2a0233f85a08571bbb1d5f371a
WairimuMaringa <makena96wairimu@gmail.com>
2023-04-06 05:48:18 -0700

    task five

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Script creating a trigger that resets an attribute
+DELIMITER $$
+CREATE TRIGGER email_trigger
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+IF NEW.email <> OLD.email
+THEN
+SET NEW.valid_email = 0;
+END IF;
+END
+$$
+DELIMITER ;
