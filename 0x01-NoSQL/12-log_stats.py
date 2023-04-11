864bbe5c2599a4510681a20f49637c128f852730
WairimuMaringa <makena96wairimu@gmail.com>
2023-04-11 07:45:52 -0700

    twelve

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collectionn = client.logs.nginx
-    print("{} logs".format(col.estimated_document_count()))
+    print("{} logs".format(collectionn.estimated_document_count()))
     print("Methods:")
 
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
