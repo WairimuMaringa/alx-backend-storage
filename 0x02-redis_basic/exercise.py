87c70b860dcf68ca3291e03938834520b5e71d02
WairimuMaringa <makena96wairimu@gmail.com>
2023-04-12 12:12:37 -0700

    added more info

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,6 +9,46 @@ from functools import wraps
 UnionOfTypes = Union[str, bytes, int, float]
 
 
+def count_calls(method: Callable) -> Callable:
+    """ Count no of times cache is called. """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ the wrapper function. """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """ History of inputs and outputs. """
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ the wrapper function. """
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(data))
+        return data
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """ Replays function history. """
+    name = method.__qualname__
+    cache = redis.Redis()
+    calls = cache.get(name).decode("utf-8")
+    print("{} was called {} times:".format(name, calls))
+    inputs = cache.lrange(name + ":inputs", 0, -1)
+    outputs = cache.lrange(name + ":outputs", 0, -1)
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(name, i.decode('utf-8'),
+                                     o.decode('utf-8')))
+
+
 class Cache:
     """ cache class. """
     def __init__(self):
@@ -18,11 +58,12 @@ class Cache:
 
     @count_calls
     @call_history
-    def store(self, data: UnionOfTypes) -> str:
+    def store(self,
+              data: UnionOfTypes) -> str:
         """ Store data. """
-        key = str(uuid4())
-        self._redis.mset({key: data})
-        return key
+        randomkey = str(uuid4())
+        self._redis.mset({randomkey: data})
+        return randomkey
 
     def get(self,
             key: str,
