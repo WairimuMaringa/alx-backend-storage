537f4edba861064015677b67a1c6c94116fc9ad6
WairimuMaringa <makena96wairimu@gmail.com>
2023-04-12 11:39:13 -0700

    added class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,4 +9,32 @@ from functools import wraps
 UnionOfTypes = Union[str, bytes, int, float]
 
 
-def
+class Cache:
+    """ cache class. """
+    def __init__(self):
+        """ Reddis db. """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @count_calls
+    @call_history
+    def store(self, data: UnionOfTypes) -> str:
+        """ Store data. """
+        key = str(uuid4())
+        self._redis.mset({key: data})
+        return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> UnionOfTypes:
+        """ Get data. """
+        data = self._redis.get(key)
+        return fn(data) if fn else data
+
+    def get_str(self, data: str) -> str:
+        """ Get string. """
+        return self.get(key, str)
+
+    def get_int(self, data: str) -> int:
+        """ Get int. """
+        return self.get(key, int)
